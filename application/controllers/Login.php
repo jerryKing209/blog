206ad8ac06fa91e2d691a82c0c5b26d2d04b8c03
Jerry <yanpeng@qutoutiao.net>
2019-10-26 20:26:51 +0800

    修改site

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -36,7 +36,7 @@ class Login extends CI_Controller{
 		//设置表单字段过滤规则
 		$this->form_validation->set_rules('username','用户名','required',array('required'=>'用户名不能为空'));
 		$this->form_validation->set_rules('password','密码','required',array('required'=>'密码不能为空'));
-		$this->form_validation->set_rules('checknum','验证码','required',array('required'=>'验证码不能为空'));
+		//$this->form_validation->set_rules('checknum','验证码','required',array('required'=>'验证码不能为空'));
 		//判断过滤结果是否成功
 		if($this->form_validation->run()) {
 			//获取用户输入信息
