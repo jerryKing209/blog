4a299ea4aa6988517cf0ecc2e8920b5c385940f6
Jerry <yanpeng@qutoutiao.net>
2019-10-26 21:01:40 +0800

    修改site

--- application/views/Admin/header.php
+++ application/views/Admin/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <title>HXC后台管理</title>
+  <title>Blog后台管理</title>
   <link rel="stylesheet" href="<?php echo base_url();?>/public/theme/layui/css/layui.css">
   <link rel="stylesheet" href="<?php echo base_url();?>/public/theme/css/admin.css">
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
