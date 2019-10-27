f45a1cab7b912362c50aca537ac069b632563951
Jerry <yanpeng@qutoutiao.net>
2019-10-27 20:16:40 +0800

    修改site

--- application/views/Admin/edit.php
+++ application/views/Admin/edit.php
@@ -43,9 +43,9 @@
 					</div>
 				</div>
 		    <!-- 配置文件 -->
-		    <script type="text/javascript" src="<?php echo base_url();?>/public/ueditor/ueditor.config.js"></script>
+		    <script type="text/javascript" src="<?php echo base_url();?>public/ueditor2/ueditor.config.js"></script>
 		    <!-- 编辑器源码文件 -->
-		    <script type="text/javascript" src="<?php echo base_url();?>/public/ueditor/ueditor.all.js"></script>
+		    <script type="text/javascript" src="<?php echo base_url();?>public/ueditor2/ueditor.all.js"></script>
 		    <!-- 实例化编辑器 -->
 		    <script type="text/javascript">
 				var ue = UE.getEditor('container',{
