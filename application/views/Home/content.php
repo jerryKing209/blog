45d83081c9aa8e8529ed10aeb1d88898ea62dbef
Jerry <yanpeng@qutoutiao.net>
2019-10-29 16:46:59 +0800

    add register

--- application/views/Home/content.php
+++ application/views/Home/content.php
@@ -88,8 +88,8 @@
                 data: $('#form1').serialize(),
                 success: function (result) {
                     console.log(result);//打印服务端返回的数据(调试用)
-                    result = JSON.stringify(result);
-                    result = JSON.parse(result);
+                    //result = JSON.stringify(result);
+                    //result = JSON.parse(result);
                     alert(result.msg);
                     if (result.status == true) {
 
