5e37cdbf6dff69424199ef3d2b34323d97c0027b
Jerry <yanpeng@qutoutiao.net>
2019-10-30 17:00:13 +0800

    add log

--- application/views/Home/header.php
+++ application/views/Home/header.php
@@ -37,8 +37,8 @@
 
             }}?>
     <?php if ($is_login == 0) {?>
-        <li id="nav_bar_li2" ><a href="<?php echo site_url('Login/index')?>">登录</a></li>
         <li id="nav_bar_li2" ><a href="<?php echo site_url('Login/register')?>">注册</a></li>
+        <li id="nav_bar_li2" ><a href="<?php echo site_url('Login/index')?>">登录</a></li>
         <?php } else {?>
         <!-- <li id="nav_bar_li3" ><img class="img-circle" src = "<?php echo base_url();?><?php echo $userinfo['photo']?>"></li> -->
         <!--其他栏目结束-->
